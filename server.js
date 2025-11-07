45c32734fe8d76a9748ffb785e99a8dd3468e11a
IsmailIbrahimi <134612116+IsmailIbrahimi@users.noreply.github.com>
2025-11-07 10:14:31 +0100

    Add of Swagger

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./src/routes/todoRoutes.js";
 import { initBackend } from "./src/config/init.js";
+// import swaggerUi from "swagger-ui-express";
+// import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 dotenv.config();
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/todos", routes);
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API prête sur http://localhost:${PORT}`));
